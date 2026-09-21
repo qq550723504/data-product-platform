8426b7d49c89d4490e10b7a28adf728d371d92b6
wei xu <xuweixia@live.com>
2026-09-21 17:34:54 +0800

    test(routing): cover delegation-chain creation event

--- apps/platform/internal/platform/routing/routing_test.go
+++ apps/platform/internal/platform/routing/routing_test.go
@@ -56,6 +56,7 @@ var eventVocabulary = []string{
 	"ExecutionSucceeded",
 	"EffectiveRightsCalculated",
 	"EffectiveRightsFinalized",
+	"GrantorAuthorityDelegationChainCreated",
 	"GrantorAuthorityDelegationChainFinalized",
 	"GrantorAuthorityDelegationInvalidated",
 	"GrantorAuthorityDelegationRevoked",
