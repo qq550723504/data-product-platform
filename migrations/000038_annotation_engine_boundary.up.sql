8af2f7f60c38dcad3ec643388143858b7879bd5a
wei xu <xuweixia@live.com>
2026-09-23 22:35:07 +0800

    fix(annotation): repair operation manifest check syntax (#205)

--- migrations/000038_annotation_engine_boundary.up.sql
+++ migrations/000038_annotation_engine_boundary.up.sql
@@ -32,7 +32,10 @@ CREATE TABLE annotation_engine_operation (
     ),
     CONSTRAINT ck_annotation_engine_operation_request CHECK (
         length(btrim(request_id)) > 0
-        AND request_fingerprint ~ '^[0-9a-f]{64}
+        AND request_fingerprint ~ '^[0-9a-f]{64}$'
+        AND payload_manifest_sha256 ~ '^[0-9a-f]{64}$'
+        AND encode(digest(payload_manifest_hash_payload, 'sha256'), 'hex') = payload_manifest_sha256
+    ),
     CONSTRAINT ck_annotation_engine_operation_claim CHECK (
         (claimed_by IS NULL AND claim_expires_at IS NULL)
         OR (length(btrim(claimed_by)) > 0 AND claim_expires_at IS NOT NULL)
