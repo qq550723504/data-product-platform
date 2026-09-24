aebe6c7682c7a33d36bc82c48ac7991e86bee1c6
wei xu <xuweixia@live.com>
2026-09-24 09:42:51 +0800

    test(routing): witness manual annotation engine resolution (#205)

--- apps/platform/internal/platform/routing/routing_test.go
+++ apps/platform/internal/platform/routing/routing_test.go
@@ -24,6 +24,7 @@ var eventVocabulary = []string{
 	"AnnotationCampaignActivated",
 	"AnnotationCampaignCreated",
 	"AnnotationEngineAttemptStarted",
+	"AnnotationEngineManualResolutionRequired",
 	"AnnotationEngineOperationPrepared",
 	"AnnotationEngineOperationResolved",
 	"AnnotationResultRecorded",
