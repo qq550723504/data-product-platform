91b1aa51108b9873dbcbc6e53f761b91d5ee6a22
wei xu <xuweixia@live.com>
2026-09-24 09:33:51 +0800

    style(annotation): gofmt pre-send retry test (#205)

--- apps/platform/internal/annotation/application/engine_test.go
+++ apps/platform/internal/annotation/application/engine_test.go
@@ -66,7 +66,6 @@ func TestResolutionStatusMapsProviderConflict(t *testing.T) {
 	}
 }
 
-
 func TestResolutionStatusRetriesDefinitePreSendFailure(t *testing.T) {
 	err := NewAnnotationEngineOutcomeError(
 		ErrAnnotationEngineUnavailable,
