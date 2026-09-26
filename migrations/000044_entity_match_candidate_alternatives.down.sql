8857717250c4ec1f35540a9f7b3a076416f30650
wei xu <xuweixia@live.com>
2026-09-26 23:07:50 +0800

    feat(entity): drop alternatives freeze trigger on rollback

--- migrations/000044_entity_match_candidate_alternatives.down.sql
+++ migrations/000044_entity_match_candidate_alternatives.down.sql
@@ -1,3 +1,6 @@
+DROP TRIGGER IF EXISTS trg_entity_match_candidate_alternatives_immutable ON entity_match_candidate;
+DROP FUNCTION IF EXISTS prevent_entity_match_candidate_alternatives_mutation();
+
 ALTER TABLE entity_match_candidate
     DROP CONSTRAINT IF EXISTS ck_entity_match_candidate_alternatives_array;
 
